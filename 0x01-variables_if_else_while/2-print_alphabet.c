ea71b0fb249be7d3dceb586808035c8164614817
Andrews Nartey <andrewsnartey9@gmail.com>
2023-02-17 17:51:27 -0800

    Project

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -11,7 +11,7 @@ int main(void)
 
 	c = 'a';
 	while
-		(c <= 'z') {
+		(c <= 'z'){
 			putchar(c);
 			c++;
 		}
